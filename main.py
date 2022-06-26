f7cca7ca246467aa1889b5a370e4fd2c61931cb5
Leo <xralendo@gmail.com>
2022-06-26 22:07:53 +0200

    70% of Fire Storm

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 # Входные данные блока стистики
 amount_attack_mage, amount_healer_mage, amount_attack_of_dragon = map(int, input().split())
 amount_mage = amount_attack_mage+amount_healer_mage
@@ -8,38 +6,63 @@ healpoint_mage, speed_of_mage, healpoint_dragon = map(int, input().split())
 # Статблок ауры магов: 0 - макс. мощность ауры, 1 - скорость роста мощности ауры, 2 - скорость уменьшения мощности ауры
 statblock_aura = [[int (i) for i in input().split()] for j in range(amount_mage)]
 
-# Статблок текущих хп и мощности ауры (текущие хп, мощность аура)
+# Статблок текущих хп и ауры [! В начале идут дамагеры, после - хиллеры !]
 statblock_real_mage = []
-for i in range((amount_mage)):
+for i in range(amount_mage):
     if i < amount_mage:
         statblock_real_mage.append([healpoint_mage, 0])
     else:
         statblock_real_mage.append([healpoint_mage, statblock_aura[i][0]])
 
-# Позиция магов (3 столбец (0 или 1) обозначает, находятся ли маги в безопасной зоне ( 0 - нет, 1 - да)
-position_of_mage = [[0] * 3 for i in range(amount_mage)]
+# Позиция магов
+position_of_mage = [[0] * 2 for i in range(amount_mage)]
 
 # Событие - Атака "Огненный смерч"
 def storm_attack():
-    damage, time_attack = map(int, input().split())
+    global healpoint_dragon
+    damage_dragon, time_attack = map(int, input().split())
     safe_zone_x, safe_zone_y = map(int, input().split())
+    heal = 0  # Общий хил магов
+    damage_mage = 0 # Общий урон магов
+    mage_aura_full = [i for i in range(amount_mage)] # Список, в котором хранятся индексы магов, аура которых не полная
+    mage_in_safe_zone = [0 for i in range(amount_mage)] # Список, где лежит индекс состояния магов (0 - не в сейф зоне, 1 - в сейф зоне)
+    max_aura, speed_up_aura, speed_down_aura = statblock_aura[i][0], statblock_aura[i][1], statblock_aura[i][2]
     while time_attack > 0:
-        # Проверка "Маг в сейф зоне? Если да - то урон дракону/хилл магов. Если нет - передвижение и урон магам"
-        for i in range(amount_mage):
-            max_aura, speed_up_aura, speed_down_aura = statblock_aura[i][0], statblock_aura[i][1], statblock_aura[i][2]
-            position_of_mage[i][2] = 0 # Обнуление условия "Маг в сейф зоне"
-            if (position_of_mage[i][2] == 1) | ((position_of_mage[i][0] == safe_zone_x) & (position_of_mage[i][1] == safe_zone_y)):
-                position_of_mage[i][2] = 1
-                #  Повышение мощности ауры атак. магов
-                if statblock_real_mage[i][1] < max_aura:
-                    if (statblock_real_mage[i][1] + speed_up_aura) > max_aura:
-                        statblock_real_mage[i][1] = max_aura
+        # Проверка "Маг в сейф зоне? Если да - то урон дракону/лечение магов. Если нет - передвижение и урон магам"
+        j = 0
+        if mage_in_safe_zone.count(0) > 0:
+            while j < len(mage_aura_full):
+                i = mage_aura_full[j]
+                # Если маг в сейф зоне, то обновление индекса состояния
+                if (position_of_mage[i][0] == safe_zone_x) & (position_of_mage[i][1] == safe_zone_y): mage_in_safe_zone[i] = 1
+                # Если маг в сейф зоне, то урон дракону и лечение магов, если нет - то урон по магам, передвижение, и снижение мощности ауры
+                if mage_in_safe_zone[i] == 1:
+                    #  Повышение мощности ауры магов
+                    if statblock_real_mage[i][1] < max_aura:
+                        if (statblock_real_mage[i][1] + speed_up_aura) > max_aura:
+                            statblock_real_mage[i][1] = max_aura
+                            mage_aura_full.pop(j)
+                            j -= 1
+                        else:
+                            statblock_real_mage[i][1] += speed_up_aura
+                    # Подсчёт общего урона и лечения магов
+                    if i < amount_attack_mage:
+                        damage_mage += statblock_real_mage[i][1]
                     else:
-                        statblock_real_mage[i][1]
+                        heal += statblock_real_mage[i][1]
+                    j += 1
+                else:
+                    # Урон по магу
+                    statblock_real_mage[i][0] -= damage_dragon
+                    # Понижение мощности ауры магов
+                    if statblock_real_mage[i][1] > 0:
+                        if (statblock_real_mage[i][1] - speed_down_aura) < 0:
+                            statblock_real_mage[i][1] = 0
+                        else:
+                            statblock_real_mage[i][1] -= speed_down_aura
+                    # Передвижение
 
 
-            else:
-                statblock_real_mage[i][0] -= damage
 
 
         time_attack -= 1
