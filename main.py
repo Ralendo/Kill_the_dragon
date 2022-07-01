6ccb9d776f5fc5276017a6b0a2bbb068c71bc961
Leo <xralendo@gmail.com>
2022-07-01 02:13:23 +0200

    Complete, repair some problems Try # 2

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 import math
-
 import numpy as np
+import colorama
+colorama.init()
 
 # Входные данные блока стистики
 amount_damager_mage, amount_healer_mage, amount_attack_of_dragon = map(int, input().split())
+# amount_damager_mage, amount_healer_mage, amount_attack_of_dragon = 1, 1, 2
+print('\r')
 amount_mage = amount_damager_mage + amount_healer_mage
-healpoint_mage, speed_of_mage, healpoint_dragon = map(int, input().split())
 
+healpoint_mage, speed_of_mage, healpoint_dragon = map(int, input().split())
+# healpoint_mage, speed_of_mage, healpoint_dragon = 1, 5, 10
+print('\r')
 # Статблок ауры магов.
 # Столбцы: 0 - макс. мощность ауры, 1 - скорость роста мощности ауры, 2 - скорость уменьшения мощности ауры
 # Дамагеры
-statblock_damager_mage_aura = [list(map(int, input().split())) for j in range(amount_damager_mage)]
+statblock_damager_mage_aura = [list(map(int, input().split())) for i in range(amount_damager_mage)]
+print('\r')
+# statblock_damager_mage_aura = [[1,3,2]]
 statblock_damager_mage_aura = np.array(statblock_damager_mage_aura)
+# print(statblock_damager_mage_aura)
 # Хилеры
 statblock_healer_mage_aura = [list(map(int, input().split())) for j in range(amount_healer_mage)]
+print('\r')
+# statblock_healer_mage_aura = [[1,1,1]]
 statblock_healer_mage_aura = np.array(statblock_healer_mage_aura)
-
+# print(statblock_healer_mage_aura)
 # Статблок текущих хп и текущей ауры магов
-statblock_real_damager_mage = np.array([healpoint_mage, 0] * amount_damager_mage)
-statblock_real_healer_mage = np.hstack((np.zeros((amount_healer_mage, 1), dtype=int), statblock_healer_mage_aura[:, 1]))
+statblock_real_damager_mage = np.array([[healpoint_mage, 0] * amount_damager_mage])
+statblock_real_healer_mage = np.hstack((np.array([[healpoint_mage] * amount_healer_mage], dtype=int), statblock_healer_mage_aura[:, 1].reshape(1, amount_healer_mage)))
+# print(statblock_real_damager_mage)
+# print(statblock_real_healer_mage)
 
 # Позиция магов в виде векторов
-position_damager_mage = np.zeros(amount_damager_mage, 2)
-position_healer_mage = np.zeros(amount_healer_mage, 2)
+position_damager_mage = np.zeros((amount_damager_mage, 2))
+position_healer_mage = np.zeros((amount_healer_mage, 2))
 
 
 # Действие магов - Атака и Лечение
-def aura_online(i, k, current_aura, mage_arrive_safezone, max_aura, speed_up_aura, total_damage, total_heal,
+def aura_online(i, k, current_aura, mage_not_full_aura, max_aura, speed_up_aura, total_damage, total_heal,
                 type_of_action):
     global statblock_real_damager_mage, statblock_real_healer_mage, healpoint_dragon
     #  Повышение мощности ауры магов
@@ -38,16 +50,13 @@ def aura_online(i, k, current_aura, mage_arrive_safezone, max_aura, speed_up_aur
                 total_damage += max_aura
             if type_of_action == 'Heal':
                 total_heal += max_aura
-            mage_arrive_safezone.delete(mage_arrive_safezone, i)
+            mage_not_full_aura = np.delete(mage_not_full_aura, i)
             k += 1
     # Нанесение урона и лечение магов
     if type_of_action == 'Damage':
-        healpoint_dragon -= current_aura
-        return [mage_arrive_safezone, k, current_aura, total_damage]
+        return [mage_not_full_aura, k, current_aura, total_damage]
     if type_of_action == 'Heal':
-        statblock_real_damager_mage += np.array([current_aura, 0] * amount_damager_mage)
-        statblock_real_healer_mage += np.array([current_aura, 0] * amount_healer_mage)
-        return [mage_arrive_safezone, k, current_aura, total_heal]
+        return [mage_not_full_aura, k, current_aura, total_heal]
 
 
 # Действие магов - Передвижение
@@ -71,18 +80,22 @@ def aura_offline(finish_position, damage_dragon, current_hp, position_of_mage, c
 def storm_attack():
     global healpoint_dragon, position_damager_mage, position_healer_mage, statblock_real_damager_mage, statblock_real_healer_mage
     damage_dragon, time_attack = map(int, input().split())
+    time_attack -= 1
+    print('\r')
     finish_position = np.array(list(map(int, input().split())))  # Точка безопасной зоны
+    # finish_position = np.array([0, 0])
+    print('\r')
     total_damage = 0  # Общий урон магов, имеющие полную ауру
     total_heal = 0  # Общий хил магов, имеющие полную ауру
-    mage_damager_aura_not_full, mage_damager_not_in_safe_zone = np.arange(amount_damager_mage)
-    mage_healer_aura_not_full, mage_healer_not_in_safe_zone = np.arange(amount_healer_mage)
+    mage_damager_aura_not_full, mage_damager_not_in_safe_zone = np.arange(amount_damager_mage), np.arange(amount_damager_mage)
+    mage_healer_aura_not_full, mage_healer_not_in_safe_zone = np.arange(amount_healer_mage), np.arange(amount_healer_mage)
     # /|\ 1 -ый Список, в котором хранятся индексы магов, аура которых не полная
     #  |  2 -ый Список, где лежат индексы магов вне сейф зоны
     while time_attack > 0:
         # Проверка "Маг с полной аурой? Если да - то запуск цикла с проверка "Маг в сейф зоне?"
         # Если да - то урон дракону/лечение магов. Если нет - передвижение и урон магам"
         counter = 0
-        if mage_damager_aura_not_full | mage_healer_aura_not_full:
+        if (mage_damager_aura_not_full.size > 0) | (mage_healer_aura_not_full.size > 0):
             amount_damager_in_safezone, amount_healer_in_safezone = 0, 0
             # Итерация сразу по двум спискам - дамагеры и хилеры
             while counter < max(mage_damager_aura_not_full.size, mage_healer_aura_not_full.size):
@@ -94,14 +107,14 @@ def storm_attack():
                                                                statblock_healer_mage_aura[i, 1], \
                                                                statblock_healer_mage_aura[i, 2]
                     # Если маг в сейф зоне, то удаление из списка индексов магов вне сейф зоны
-                    if position_healer_mage[i] == finish_position:
-                        mage_healer_not_in_safe_zone.delete(mage_healer_not_in_safe_zone, i)
+                    if np.all(position_healer_mage[i] == finish_position):
+                        mage_healer_not_in_safe_zone = np.delete(mage_healer_not_in_safe_zone, i)
                     # Если маг в сейф зоне, то усиление ауры и лечение магов, если нет - то урон по магам, передвижение, и снижение мощности ауры
                     if i not in mage_healer_not_in_safe_zone:
                         mage_healer_aura_not_full, amount_healer_in_safezone, statblock_real_healer_mage[
                             i, 1], total_heal = aura_online(i, amount_healer_in_safezone,
                                                             statblock_real_healer_mage[i, 1], mage_healer_aura_not_full,
-                                                            max_aura, speed_up_aura, None, total_heal, "Heal")
+                                                            max_aura, speed_up_aura, total_damage, total_heal, "Heal")
                     else:
                         statblock_real_healer_mage[i, 0], position_healer_mage[i], statblock_real_healer_mage[
                             i, 1] = aura_offline(finish_position, damage_dragon, statblock_real_healer_mage[i, 0],
@@ -115,15 +128,15 @@ def storm_attack():
                                                                statblock_damager_mage_aura[i, 1], \
                                                                statblock_damager_mage_aura[i, 2]
                     # Если маг в сейф зоне, то удаление из списка индексов магов вне сейф зоны
-                    if position_damager_mage[i] == finish_position:
-                        mage_damager_not_in_safe_zone.delete(mage_damager_not_in_safe_zone, i)
+                    if np.all(position_damager_mage[i] == finish_position):
+                        mage_damager_not_in_safe_zone = np.delete(mage_damager_not_in_safe_zone, i)
                     # Если маг в сейф зоне, то урон магов и усиление ауры, если нет - то урон по магам, передвижение, и снижение мощности ауры
                     if i not in mage_damager_not_in_safe_zone:
                         mage_damager_aura_not_full, amount_damager_in_safezone, statblock_real_healer_mage[
                             i, 1], total_damage = aura_online(i, amount_healer_in_safezone,
                                                               statblock_real_healer_mage[i, 1],
-                                                              mage_healer_aura_not_full, max_aura,
-                                                              speed_up_aura, total_damage, None, "Damage")
+                                                              mage_damager_aura_not_full, max_aura,
+                                                              speed_up_aura, total_damage, total_heal, "Damage")
                     else:
                         statblock_real_damager_mage[i, 0], position_damager_mage[i], statblock_real_damager_mage[
                             i, 1] = aura_offline(finish_position, damage_dragon, statblock_real_damager_mage[i, 0],
@@ -142,11 +155,11 @@ def storm_attack():
         statblock_real_healer_mage += np.array([total_heal, 0] * amount_healer_mage)
         statblock_real_healer_mage = np.clip(statblock_real_healer_mage, 0, healpoint_mage)
 
-        if 0 in statblock_real_healer_mage[:, 0] | 0 in statblock_real_damager_mage[:, 0]:
+        if (statblock_real_healer_mage[:, 0].count(0) == 0) | statblock_real_damager_mage[:, 0].count(0) == 0:
             print("You are not prepared")
             exit()
 
-        if healpoint_dragon <= 0 & (not (mage_healer_not_in_safe_zone | mage_damager_not_in_safe_zone)):
+        if healpoint_dragon <= 0 & ((mage_damager_not_in_safe_zone.size == 0) & (mage_damager_not_in_safe_zone.size == 0)):
             print("No useful loot again")
             print(counter_of_attack)
             exit()
@@ -157,12 +170,15 @@ def storm_attack():
 def breath_attack():
     global healpoint_dragon, position_damager_mage, position_healer_mage, statblock_real_damager_mage, statblock_real_healer_mage
     damage_dragon, time_attack = map(int, input().split())
+    time_attack =- 1
+    print('\r')
     safe_positions = [list(map(int, input().split())) for j in range(amount_mage)]
+    print('\r')
     safe_positions = np.array(safe_positions)
     total_damage = 0  # Общий урон магов, имеющие полную ауру
     total_heal = 0  # Общий хил магов, имеющие полную ауру
-    mage_damager_aura_not_full, mage_damager_not_in_safe_zone = np.arange(amount_damager_mage)
-    mage_healer_aura_not_full, mage_healer_not_in_safe_zone = np.arange(amount_healer_mage)
+    mage_damager_aura_not_full, mage_damager_not_in_safe_zone = np.arange(amount_damager_mage), np.arange(amount_damager_mage)
+    mage_healer_aura_not_full, mage_healer_not_in_safe_zone = np.arange(amount_healer_mage), np.arange(amount_healer_mage)
     # /|\ 1 -ый Список, в котором хранятся индексы магов, аура которых не полная
     #  |  2 -ый Список, где лежат индексы магов вне сейф зоны
 
@@ -349,10 +365,10 @@ def breath_attack():
     safe_positions = np.array(booked_safe_position)
     # Ежесекундные ходы
     while time_attack > 0:
-        # Проверка "Маг с полной аурой? Если да - то запуск цикла с проверкой "Маг в сейф зоне?"
+        # Проверка "Маг с полной аурой? Если да - то запуск цикла с проверка "Маг в сейф зоне?"
         # Если да - то урон дракону/лечение магов. Если нет - передвижение и урон магам"
         counter = 0
-        if mage_damager_aura_not_full | mage_healer_aura_not_full:
+        if (mage_damager_aura_not_full.size > 0) | (mage_healer_aura_not_full.size > 0):
             amount_damager_in_safezone, amount_healer_in_safezone = 0, 0
             # Итерация сразу по двум спискам - дамагеры и хилеры
             while counter < max(mage_damager_aura_not_full.size, mage_healer_aura_not_full.size):
@@ -364,14 +380,14 @@ def breath_attack():
                                                                statblock_healer_mage_aura[i, 1], \
                                                                statblock_healer_mage_aura[i, 2]
                     # Если маг в сейф зоне, то удаление из списка индексов магов вне сейф зоны
-                    if position_healer_mage[i] == safe_positions[i]:
-                        mage_healer_not_in_safe_zone.delete(mage_healer_not_in_safe_zone, i)
+                    if np.all(position_healer_mage[i] == safe_positions[i]):
+                        mage_healer_not_in_safe_zone = np.delete(mage_healer_not_in_safe_zone, i)
                     # Если маг в сейф зоне, то усиление ауры и лечение магов, если нет - то урон по магам, передвижение, и снижение мощности ауры
                     if i not in mage_healer_not_in_safe_zone:
                         mage_healer_aura_not_full, amount_healer_in_safezone, statblock_real_healer_mage[
                             i, 1], total_heal = aura_online(i, amount_healer_in_safezone,
                                                             statblock_real_healer_mage[i, 1], mage_healer_aura_not_full,
-                                                            max_aura, speed_up_aura, None, total_heal, "Heal")
+                                                            max_aura, speed_up_aura, total_damage, total_heal, "Heal")
                     else:
                         statblock_real_healer_mage[i, 0], position_healer_mage[i], statblock_real_healer_mage[
                             i, 1] = aura_offline(safe_positions[i], damage_dragon, statblock_real_healer_mage[i, 0],
@@ -385,15 +401,15 @@ def breath_attack():
                                                                statblock_damager_mage_aura[i, 1], \
                                                                statblock_damager_mage_aura[i, 2]
                     # Если маг в сейф зоне, то удаление из списка индексов магов вне сейф зоны
-                    if position_damager_mage[i] == safe_positions[i]:
-                        mage_damager_not_in_safe_zone.delete(mage_damager_not_in_safe_zone, i)
+                    if np.all(position_damager_mage[i] == safe_positions[i]):
+                        mage_damager_not_in_safe_zone = np.delete(mage_damager_not_in_safe_zone, i)
                     # Если маг в сейф зоне, то урон магов и усиление ауры, если нет - то урон по магам, передвижение, и снижение мощности ауры
                     if i not in mage_damager_not_in_safe_zone:
                         mage_damager_aura_not_full, amount_damager_in_safezone, statblock_real_healer_mage[
                             i, 1], total_damage = aura_online(i, amount_healer_in_safezone,
                                                               statblock_real_healer_mage[i, 1],
-                                                              mage_healer_aura_not_full, max_aura,
-                                                              speed_up_aura, total_damage, None, "Damage")
+                                                              mage_damager_aura_not_full, max_aura,
+                                                              speed_up_aura, total_damage, total_heal, "Damage")
                     else:
                         statblock_real_damager_mage[i, 0], position_damager_mage[i], statblock_real_damager_mage[
                             i, 1] = aura_offline(safe_positions[i], damage_dragon, statblock_real_damager_mage[i, 0],
@@ -412,10 +428,15 @@ def breath_attack():
         statblock_real_healer_mage += np.array([total_heal, 0] * amount_healer_mage)
         statblock_real_healer_mage = np.clip(statblock_real_healer_mage, 0, healpoint_mage)
 
-        if 0 in statblock_real_healer_mage[:, 0] | 0 in statblock_real_damager_mage[:, 0]:
+        if (statblock_real_healer_mage[:, 0].count(0) == 0) | statblock_real_damager_mage[:, 0].count(0) == 0:
             print("You are not prepared")
             exit()
 
+        if healpoint_dragon <= 0 & (
+                (mage_damager_not_in_safe_zone.size == 0) & (mage_damager_not_in_safe_zone.size == 0)):
+            print("No useful loot again")
+            print(counter_of_attack)
+            exit()
         time_attack -= 1
 
 
@@ -430,6 +451,10 @@ counter_of_attack = 0
 for kill_the_mage in range(amount_attack_of_dragon):
     counter_of_attack += 1
     type_attack = input()
-    if type_attack == "Tail": tail()
-    if type_attack == "Storm": storm_attack()
-    if type_attack == "Breath": breath_attack()
+    print('\r')
+    if type_attack == "Tail":
+        tail()
+    if type_attack == "Storm":
+        storm_attack()
+    if type_attack == "Breath":
+        breath_attack()
